a43714ec72e20bac044751e6e761f956392d489a
Tobias Cohen <me@tobiascohen.com>
2013-04-10 19:23:33 +1000

    Added iTerm preferences

--- link.sh
+++ link.sh
@@ -3,3 +3,10 @@ do
   rm -f $HOME/.$file
   ln -s $HOME/.dotfiles/$file $HOME/.$file
 done
+
+for file in preferences/*
+do
+  name=`basename $file`
+  rm -f $HOME/Library/Preferences/$name
+  ln -s $HOME/.dotfiles/$file $HOME/Library/Preferences/$name
+done
