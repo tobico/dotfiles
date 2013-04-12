ba375ddaea3bb6147769d986cc5bf08dc63ce0b7
Tobias Cohen <me@tobiascohen.com>
2013-04-13 09:55:18 +1000

    Don't store iTerm preferences

--- link.sh
+++ link.sh
@@ -3,10 +3,3 @@ do
   rm -f $HOME/.$file
   ln -s $HOME/.dotfiles/$file $HOME/.$file
 done
-
-for file in preferences/*
-do
-  name=`basename $file`
-  rm -f $HOME/Library/Preferences/$name
-  ln -s $HOME/.dotfiles/$file $HOME/Library/Preferences/$name
-done
