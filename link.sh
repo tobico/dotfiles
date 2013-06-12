e725ec4ea84877a953e5b379486a17d66a7b7e75
Tobias Cohen <me@tobiascohen.com>
2013-06-12 13:10:11 +1000

    Include Sublime packages

--- link.sh
+++ link.sh
@@ -3,3 +3,6 @@ do
   rm -f $HOME/.$file
   ln -s $HOME/.dotfiles/$file $HOME/.$file
 done
+
+rm -rf $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
+ln -s $HOME/.dotfiles/sublime_packages $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
