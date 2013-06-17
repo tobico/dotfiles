e7c3c76adb93a942b6769517ab7d204857017373
Tobias Cohen <me@tobiascohen.com>
2013-06-17 10:24:22 +1000

    fixed bug in link script

--- link.sh
+++ link.sh
@@ -1,10 +1,11 @@
 for file in zshrc gitconfig
 do
-  rm -f $HOME/.$file
-  ln -s $HOME/.dotfiles/$file $HOME/.$file
+  rm -fv $HOME/.$file
+  ln -sv $HOME/.dotfiles/$file $HOME/.$file
 done
 
-rm -rf $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
-ln -s $HOME/.dotfiles/sublime_packages $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
-rm -f $HOME/Library/Keybindings/DefaultKeyBinding.dict
-ln -s $HOME/.dotfiles/key_bindings $HOME/Library/Keybindings/DefaultKeyBinding.dict
+rm -rfv $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
+ln -sv $HOME/.dotfiles/sublime_packages $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
+rm -fv $HOME/Library/Keybindings/DefaultKeyBinding.dict
+mkdir -pv $HOME/Library/Keybindings
+ln -sv $HOME/.dotfiles/key_bindings $HOME/Library/Keybindings/DefaultKeyBinding.dict
