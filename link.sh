0e0fc90a536cdcf3184ea2a826856ecdc474f256
Tobias Cohen <me@tobiascohen.com>
2013-08-23 10:41:14 +1000

    Added Tmux config

--- link.sh
+++ link.sh
@@ -1,11 +1,9 @@
-for file in zshrc gitconfig
+for file in zshrc gitconfig tmux.conf
 do
   rm -fv $HOME/.$file
   ln -sv $HOME/.dotfiles/$file $HOME/.$file
 done
 
-rm -rfv $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
-ln -sv $HOME/.dotfiles/sublime_packages $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages
 rm -fv $HOME/Library/Keybindings/DefaultKeyBinding.dict
 mkdir -pv $HOME/Library/Keybindings
 ln -sv $HOME/.dotfiles/key_bindings $HOME/Library/Keybindings/DefaultKeyBinding.dict
