ac69eeb9b5ecec93f7390e1a9165a79262c823a0
Tobias Cohen <me@tobiascohen.com>
2013-09-08 11:08:05 +1000

    Adds Services folder, with QR Codify service

--- link.sh
+++ link.sh
@@ -4,6 +4,9 @@ do
   ln -sv $HOME/.dotfiles/$file $HOME/.$file
 done
 
+rm -f $HOME/Library/Services
+ln -s $HOME/.dotfiles/services $HOME/Library/Services
+
 rm -fv $HOME/Library/Keybindings/DefaultKeyBinding.dict
 mkdir -pv $HOME/Library/Keybindings
 ln -sv $HOME/.dotfiles/key_bindings $HOME/Library/Keybindings/DefaultKeyBinding.dict
