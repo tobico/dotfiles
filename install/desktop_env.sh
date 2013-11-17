1fb6fae75e65fa5a0e31a7a27b884873dbff6f4f
Tobias Cohen <me@tobiascohen.com>
2013-11-18 09:16:18 +1100

    Adds some scripts to help set up Ubuntu

---
+++ install/desktop_env.sh
@@ -0,0 +1,5 @@
+# command line stuff
+apt-get -y install zsh vim tmux wget tig ncdu
+
+# xwindows
+apt-get -y install xserver-xorg xdm i3 chromium-browser
---
+++ install/dev_dependencies.sh
@@ -0,0 +1,8 @@
+# ruby dependencies
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+
+# nokogiri dependencies
+apt-get -y install libxslt-dev libxml2-dev
+
+# mysql2 gem dependency
+apt-get -y install libmysqlclient-dev
