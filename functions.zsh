70e0062558eebca64b1342954c6e2b1d3530f043
Tobias Cohen <me@tobiascohen.com>
2013-11-28 13:31:48 +1100

    Adds `cwd` function

--- functions.zsh
+++ functions.zsh
@@ -1,1 +1,10 @@
 ghcl () { mkdir -p ~/src && git clone git@github.com:$1/$2 ~/src/$2 }
+
+# Function to switch and save the current path
+# http://blog.eexit.net/2011/05/zsh-open-a-new-window-using-the-last-pwd.html
+function cd() {
+  builtin cd "$@";
+  echo "$PWD" > ~/.cwd;
+}
+export cd
+alias cwd='cd "$(cat ~/.cwd)"'
