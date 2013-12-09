12fe5a5989e9a859807d826670f0e59abf2fdeef
Tobias Cohen <me@tobiascohen.com>
2013-12-09 16:20:47 +1100

    Tweaks

--- functions.zsh
+++ functions.zsh
@@ -4,7 +4,7 @@ ghcl () { mkdir -p ~/src && git clone git@github.com:$1/$2 ~/src/$2 }
 # http://blog.eexit.net/2011/05/zsh-open-a-new-window-using-the-last-pwd.html
 function cd() {
   builtin cd "$@";
-  echo "$PWD" > ~/.cwd;
+  pwd > ~/.cwd;
 }
 export cd
 alias cwd='cd "$(cat ~/.cwd)"'
