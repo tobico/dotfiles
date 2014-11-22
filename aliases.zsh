38b7593dc96c41d2a2a343ebcea18caf721809e8
Tobias Cohen <me@tobiascohen.com>
2014-11-23 10:25:07 +1100

    Remove unused aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,18 +1,6 @@
-alias mercy="kill -9 %1"
 alias be="bundle exec"
 alias r="bundle exec rails"
-alias -g yo=' && echo dawg'
-alias re="touch tmp/restart.txt"
-alias hooray="echo \"Hooray! =D\""
 alias superpush="git fetch && git rebase origin/master && git push"
 alias hbs="hub browse"
 alias gdmb="git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d"
 alias ls="ls -h"
-
-alias zs="zeus start"
-alias zr="zeus rspec"
-alias zv="zeus server"
-alias zc="zeus console"
-alias zg="zeus generate"
-alias zk="zeus rake"
-alias zkt="zeus rake -T"
