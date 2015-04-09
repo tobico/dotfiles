ce9642257c6b02c00612918c720a48219600d904
Tobias Cohen <me@tobiascohen.com>
2015-04-09 17:03:10 +1000

    Updates

--- functions.zsh
+++ functions.zsh
@@ -1,4 +1,4 @@
-ghcl () { mkdir -p ~/src && git clone git@github.com:$1/$2 ~/src/$2 }
+ghcl () { mkdir -p ~/src && git clone git@github.com:$1/$2 ~/src/$1/$2 }
 
 # Function to switch and save the current path
 # http://blog.eexit.net/2011/05/zsh-open-a-new-window-using-the-last-pwd.html
