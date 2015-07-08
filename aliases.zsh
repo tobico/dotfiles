151d414802a9bcca2be74bba91dca338c5c1cf12
Tobias Cohen <me@tobiascohen.com>
2015-07-08 15:16:41 +1000

    aliases! ☃

--- aliases.zsh
+++ aliases.zsh
@@ -4,3 +4,8 @@ alias superpush="git fetch && git rebase origin/master && git push"
 alias hbs="hub browse"
 alias gdmb="git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d"
 alias ls="ls -h"
+alias -g yo="$@ && echo dawg"
+
+function dockerclean() {
+  docker rm $(docker ps -a -q)
+}
