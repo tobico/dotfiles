1d64536eca9068b22f9e8fe24a07da5f6c168894
Tobias Cohen <me@tobiascohen.com>
2016-07-04 15:32:36 +1000

    Add chrome-debug alias for Mac

--- aliases.zsh
+++ aliases.zsh
@@ -5,6 +5,7 @@ alias hbs="hub browse"
 alias gdmb="git branch --merged | grep -v \"\\*\" | xargs -n 1 git branch -d"
 alias ls="ls -h"
 alias -g yo="$@ && echo dawg"
+alias chrome-debug="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')"
 
 function dockerclean() {
   docker rm $(docker ps -a -q)
