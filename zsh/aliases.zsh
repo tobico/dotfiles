80f7b025363fe4927b2ac7a4cb2b8f67f39512e6
Tobias Cohen <me@tobiascohen.com>
2016-10-20 10:42:04 +1100

    Alias g to hub when available

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,9 @@
 alias be="bundle exec"
 alias r="bundle exec rails"
-alias g="git"
+if type hub > /dev/null; then
+  alias g="hub"
+else
+  alias g="git"
+fi
 alias ls="ls -Ghp"
 alias -g yo="$@ && echo dawg"
